90de4d4cc90538e5db7f4cbb469251200c8b3d31
Nameer Rizvi <rizvinameer@gmail.com>
2020-10-08 21:15:36 -0400

    App constants

--- pages/api/popular-searches.js
+++ pages/api/popular-searches.js
@@ -1,24 +1,26 @@
 import jsontxt from "jsontxt";
 
 export default (req, res) =>
-  req.method === "GET" &&
-  (() => {
-    const recommendations = jsontxt.read();
+  req.method === "GET"
+    ? (() => {
+        const recommendations = jsontxt.read();
 
-    const getMostFrequentVariation = (group) =>
-      group.variations.reduce(
-        (a, b, i, arr) =>
-          arr.filter((v) => v === a).length >= arr.filter((v) => v === b).length
-            ? a
-            : b,
-        group[0]
-      );
+        const getMostFrequentVariation = (group) =>
+          group.variations.reduce(
+            (a, b, i, arr) =>
+              arr.filter((v) => v === a).length >=
+              arr.filter((v) => v === b).length
+                ? a
+                : b,
+            group[0]
+          );
 
-    const payload = recommendations
-      .sort((a, b) => a.count - b.count)
-      .reverse()
-      .slice(0, 5)
-      .map(getMostFrequentVariation);
+        const payload = recommendations
+          .sort((a, b) => a.count - b.count)
+          .reverse()
+          .slice(0, 5)
+          .map(getMostFrequentVariation);
 
-    res.json(payload);
-  })();
+        res.json(payload);
+      })()
+    : res.status(400).send("Request method not supported.");
