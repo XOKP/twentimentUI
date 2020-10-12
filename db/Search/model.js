bbc713b954415397205fa143a9a96946bc254bae
Nameer Rizvi <rizvinameer@gmail.com>
2020-10-11 22:34:18 -0400

    round size of search up

--- db/Search/model.js
+++ db/Search/model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const searchSchema = new Schema(
   { string: String },
-  { capped: { size: 12750, max: 250 } }
+  { capped: { size: 15000, max: 250 } }
 );
 
 const SearchModel =
