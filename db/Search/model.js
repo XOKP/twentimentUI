39be7dbd9fe7dad7d7566068d79842a6ded96a4c
Nameer Rizvi <rizvinameer@gmail.com>
2020-10-11 22:24:36 -0400

    increase size of search collection

--- db/Search/model.js
+++ db/Search/model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const searchSchema = new Schema(
   { string: String },
-  { capped: { size: 1024, max: 250 } }
+  { capped: { size: 12750, max: 250 } }
 );
 
 const SearchModel =
